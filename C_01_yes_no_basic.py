7ba94aa60814b8c2e3707b01e0e3367202545801
SageWool <alhussainh24240@masseyhigh.school.nz>
2026-02-17 09:29:58 +1300

    Add files via upload

--- C_01_yes_no_basic.py
+++ C_01_yes_no_basic.py
@@ -1,7 +1,7 @@
 want_instructions =input("Do you want to see the instructions? "). lower()
 
 # check the user says yes / no
-if want_instructions == "yes": or want_instructions == "y":
+if want_instructions == "yes" or want_instructions == "y":
     print("you said yes")
 elif want_instructions == "no" or want_instructions == "n":
     print("you said no")
