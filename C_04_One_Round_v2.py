fa9158d71cec2adbb1741d59633a37af0b680f62
SageWool <alhussainh24240@masseyhigh.school.nz>
2026-02-26 10:41:06 +1300

    Add files via upload

--- C_04_One_Round_v2.py
+++ C_04_One_Round_v2.py
@@ -14,30 +14,35 @@ def initial_points(which_player):
         double = "yes"
 
     total = roll_one + roll_two
-
     print(f"{which_player} - Roll 1: {roll_one} \t| Roll 2: {roll_two} \t| Total: {total} ")
+
     return total, double
 
-# Initialise rounds points
-user_points = 0
-comp_points = 0
+# Main starts here...
 
 
 
-# roll the dice for the computer
-comp_one = random.randint(1, 6)
-comp_two = random.randint(1, 6)
+# Roll the dice for the user and note if they got a double
+initial_user = initial_points("User")
+initial_comp = initial_points("Comp")
 
 
-# Update the user / computer points
-user_points += user_one + user_two
-comp_points += comp_one + comp_two
+# Retrieve user points (first item returned from function)
+user_points = initial_user[0]
+comp_points = initial_comp[0]
 
-# Output the results
-print("\nInitial Points")
-print(f"User     - Roll 1: {user_one} \t| Roll 2: {user_two} \t| Total: {user_points} ")
-print(f"Computer - Roll 1: {comp_one} \t| Roll 2: {comp_two} \t| Total: {comp_points} ")
+double_user = initial_user[1]
 
 # Let the user know if they qualify for double points
-if double == "yes":
-    print("great news - if you win, you will earn double points!")
+if double_user == "yes":
+    print("Great news - if you win, you will earn double points!")
+
+# assume user goes first...
+first = "User"
+second = "Computer"
+player_1_points = user_points
+player_2_points = comp_points
+
+# if user has fewer points, they start the game
+if user_points < com
+
